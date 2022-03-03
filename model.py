80624ceff7b6433d286064f38aa09f1e618d6df7
GColom <giulio.colombini@studio.unibo.it>
2022-03-03 15:00:23 +0100

    Added Hospitalised compartment and dummy mobility values to produce a peak.

--- model.py
+++ model.py
@@ -11,17 +11,20 @@ from   scipy import stats
 from   tqdm  import tqdm
 
 # Social activity rate
-# The example is a quite strong lockdown 14 days after the introduction of patient zero.
-m_test = (np.array([0., 14, 20, np.inf]), np.array([1., .5, .15, .5]))
+# The example is a quite strong lockdown 30 days after the introduction of patient zero.
+m_test = (np.array([0, 30, 60, np.inf]), np.array([1., .15, .15, .5]))
 
-# Symptomatic fraction
-alpha_test = (np.array([0., np.inf]), np.array([.14, .14]))
+# Hospitalisation fraction of symptomatic patients
+gamma_test = (np.array([0., np.inf]), np.array([.85, .5]))
 
 # Parameters of \rho_E over time
 pars_e_test = (np.array([0., np.inf]), np.array([2.,2.]), np.array([.1,.1]))
 
 # Parameters of \rho_I over time
 pars_i_test = (np.array([0., np.inf]), np.array([5.5,5.5]), np.array([2.3,2.3]))
+
+# Parameters of \rho_H over time
+pars_h_test = (np.array([0., np.inf]), np.array([7, 7]), 0.1 * np.array([7, 7]))
 
 # Parameters of \rho_A over time
 pars_a_test = pars_i_test
@@ -59,9 +62,10 @@ def propagate_forward(t, max_t, donor, acceptors, kernel_tuple, branching_ratios
     for a, r in zip(acceptors, branching_ratios):
         a[t + i0 : t + i0 + lk] += r * buffer
 
-def run_simulation(days = 60, dt = 1./24., beta = 1/1.2, N = 886891, m = m_test, 
-                   alpha = alpha_test, pars_e = pars_e_test, 
-                   pars_i = pars_i_test, pars_a = pars_a_test):
+def run_simulation(days = 60, dt = 1./24., beta = 1/1.2, alpha = .14, 
+                    N = 886891, m = m_test, gamma = gamma_test, 
+                    pars_e = pars_e_test, pars_i = pars_i_test, 
+                    pars_h = pars_h_test, pars_a = pars_a_test):
     # Calculate number of iterations
     max_step = int(np.rint(days / dt))
     
@@ -69,13 +73,16 @@ def run_simulation(days = 60, dt = 1./24., beta = 1/1.2, N = 886891, m = m_test,
     S = np.zeros(max_step+1)
     E = np.zeros(max_step+1)
     I = np.zeros(max_step+1)
+    H = np.zeros(max_step+1)
     A = np.zeros(max_step+1)
     R = np.zeros(max_step+1)
     TOT = np.zeros(max_step+1)
     
     Phi_SE = np.zeros(max_step+1)
     Phi_EI = np.zeros(max_step+1)
+    Phi_EH = np.zeros(max_step+1)
     Phi_EA = np.zeros(max_step+1)
+    Phi_HR = np.zeros(max_step+1)
     Phi_IR = np.zeros(max_step+1)
     Phi_AR = np.zeros(max_step+1)
     
@@ -86,10 +93,10 @@ def run_simulation(days = 60, dt = 1./24., beta = 1/1.2, N = 886891, m = m_test,
     
     m_array= np.array([m_vals[np.searchsorted(m_t, t, side = 'right') - 1] for t in range(max_step+1)])
     
-    alpha_t    = alpha[0] / dt
-    alpha_vals = alpha[1]
+    gamma_t    = gamma[0] / dt
+    gamma_vals = gamma[1]
     
-    alpha_array= np.array([alpha_vals[np.searchsorted(alpha_t, t, side = 'right') - 1] for t in range(max_step+1)])
+    gamma_array= np.array([gamma_vals[np.searchsorted(gamma_t, t, side = 'right') - 1] for t in range(max_step+1)])
     
     # Unpack distribution tuples and generate distributions
     
@@ -104,6 +111,12 @@ def run_simulation(days = 60, dt = 1./24., beta = 1/1.2, N = 886891, m = m_test,
     rho_i_sigmas = pars_i[2] / dt
 
     rho_is = [discrete_gamma(mu, sigma) for mu, sigma in zip(rho_i_mus, rho_i_sigmas)]
+
+    rho_h_t      = pars_h[0] / dt
+    rho_h_mus    = pars_h[1] / dt
+    rho_h_sigmas = pars_h[2] / dt
+
+    rho_hs = [discrete_gamma(mu, sigma) for mu, sigma in zip(rho_h_mus, rho_h_sigmas)]
 
     rho_a_t      = pars_a[0] / dt
     rho_a_mus    = pars_a[1] / dt
@@ -124,6 +137,7 @@ def run_simulation(days = 60, dt = 1./24., beta = 1/1.2, N = 886891, m = m_test,
     
     cur_rho_e_idx = 0
     cur_rho_i_idx = 0
+    cur_rho_h_idx = 0
     cur_rho_a_idx = 0
     
     # Master simulation loop
@@ -131,25 +145,27 @@ def run_simulation(days = 60, dt = 1./24., beta = 1/1.2, N = 886891, m = m_test,
         # Update distribution indices
         cur_rho_e_idx = np.searchsorted(rho_e_t, t, side = 'right') - 1
         cur_rho_i_idx = np.searchsorted(rho_i_t, t, side = 'right') - 1
+        cur_rho_h_idx = np.searchsorted(rho_h_t, t, side = 'right') - 1
         cur_rho_a_idx = np.searchsorted(rho_a_t, t, side = 'right') - 1
-        
+
         # Get current parameters
         cur_m     = m_array[t]
-        cur_alpha = alpha_array[t]
-        
+        cur_gamma = gamma_array[t]
         
         # Evaluate active population
         
         P = S[t] + E[t] + A[t] + R[t]
         
         # Evolve contagion flow
-        Phi_SE[t] += beta * cur_m * S[t] * (E[t] + A[t]) * dt / P
+        Phi_SE[t] += beta * cur_m * S[t] * (A[t]) * dt / P
         
         # Propagate flows
         
-        propagate_forward(t, max_step, Phi_SE[t], [Phi_EI, Phi_EA], rho_es[cur_rho_e_idx],
-                          branching_ratios = np.array([cur_alpha, 1 - cur_alpha]))
+        propagate_forward(t, max_step, Phi_SE[t], [Phi_EI, Phi_EH, Phi_EA], rho_es[cur_rho_e_idx],
+                          branching_ratios = np.array([alpha * (1 - cur_gamma), alpha * cur_gamma, 1 - alpha]))
         propagate_forward(t, max_step, Phi_EI[t], [Phi_IR], rho_is[cur_rho_i_idx],
+                          branching_ratios = np.array([1.]))
+        propagate_forward(t, max_step, Phi_EH[t], [Phi_HR], rho_hs[cur_rho_h_idx],
                           branching_ratios = np.array([1.]))
         propagate_forward(t, max_step, Phi_EA[t], [Phi_AR], rho_as[cur_rho_a_idx],
                           branching_ratios = np.array([1.]))
@@ -157,31 +173,34 @@ def run_simulation(days = 60, dt = 1./24., beta = 1/1.2, N = 886891, m = m_test,
         # Evolve compartments
         
         S[t+1] = S[t] - Phi_SE[t]
-        E[t+1] = E[t] + Phi_SE[t] - Phi_EA[t] - Phi_EI[t]
+        E[t+1] = E[t] + Phi_SE[t] - Phi_EA[t] - Phi_EI[t] - Phi_EH[t]
         I[t+1] = I[t] + Phi_EI[t] - Phi_IR[t]
+        H[t+1] = H[t] + Phi_EH[t] - Phi_HR[t]
         A[t+1] = A[t] + Phi_EA[t] - Phi_AR[t]
-        R[t+1] = R[t] + Phi_IR[t] + Phi_AR[t]
-        TOT[t+1] = S[t+1] + E[t+1] + I[t+1] + A[t+1] + R[t+1]
+        R[t+1] = R[t] + Phi_IR[t] + Phi_AR[t] + Phi_HR[t]
+        TOT[t+1] = S[t+1] + E[t+1] + I[t+1] + A[t+1] + H[t+1] + R[t+1]
     t = np.array([t for t in range(max_step+1)])
-    return (t, S, E, I, A, R, TOT)
+    return (t, S, E, I, H, A, R, TOT)
 
-t,s,e,i,a,r, tot = run_simulation()
+t,s,e,i,h,a,r,tot = run_simulation(100)
 #%% Graphics
 from matplotlib import pyplot as plt
 
 dt = 1/24
 
-plt.plot(t * dt,s, label = 'S')
-plt.plot(t * dt,e, label = 'E')
-plt.plot(t * dt,i, label = 'I')
-plt.plot(t * dt,a, label = 'A')
-plt.plot(t * dt,r, label = 'R')
-# plt.plot(t * dt, tot, label = 'TOT')
-
+plt.plot(t * dt,s, label = 'S', linewidth = .5)
+plt.plot(t * dt,e, label = 'E', linewidth = .5)
+plt.plot(t * dt,i, label = 'I', linewidth = .5)
+plt.plot(t * dt,h, label = 'H', linewidth = .5)
+plt.plot(t * dt,a, label = 'A', linewidth = .5)
+plt.plot(t * dt,r, label = 'R', linewidth = .5)
+#plt.plot(t * dt, tot, label = 'TOT')
 
 plt.legend()
 plt.grid(True)
-plt.ylim(bottom = 0)
+plt.ylim(bottom = 0, top = 2000)
 plt.xlim([0, max(t * dt)])
 plt.xlabel('Days since patient zero introduction')
 plt.ylabel('People')
+
+plt.show()
