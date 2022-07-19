12bd1e080313c9d6da246400555e22aca85c29d3
keivan-amini <keivan.amini@outlook.it>
2022-07-19 21:10:33 +0200

    added comments.

--- sociality-vs-mobility.py
+++ sociality-vs-mobility.py
@@ -1,25 +1,21 @@
-# Sociality vs Mobility code, 2020-2022.
+# Sociality vs Mobility code. Data considered: 2020, 2021, 2022.
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 # Social activity rate
 m_vals = np.array([1.00, 1.00, 1.00, 0.24, 0.16, 0.17, 0.18, 0.24, 0.24, 0.24, 0.34, 0.24, 0.21, 0.27, 0.25, 0.23, 0.29, 0.26, 0.20, 0.39, 0.29, 0.65, 0.49, 0.35, 0.29, 0.24, 0.19, 0.25, 0.06, 0.08, 0.18, 0.21, 0.18, 0.12, 0.08, 0.06])
 m_days = np.array([0, 54, 61, 71, 83, 125, 139, 155, 167, 181, 258, 299, 320, 341, 359, 375, 398, 418, 429, 468, 482, 537, 560, 575, 610, 635, 688, 714, 727, 750, 770, 791, 810, 859, 900, 910])
-
 
 url_2020 = 'https://raw.githubusercontent.com/keivan-amini/simplified-covid-model/main/rilevazione-autoveicoli-tramite-spire-anno-2020_header_mod.csv'
 url_2021 = 'https://raw.githubusercontent.com/keivan-amini/simplified-covid-model/main/rilevazione-autoveicoli-tramite-spire-anno-2021_header_mod.csv'
 url_2022 = "https://raw.githubusercontent.com/keivan-amini/simplified-covid-model/main/rilevazione-autoveicoli-tramite-spire-anno-2022_header_mod.csv"
 
-# Function that take in input 3 strings and outputs one single mobility array.
-
 def get_total_mobility(url):
 
     '''
-    Given the link of the mobility dataframe, return the mobility.
+    Given the link of the dataframe, return an array containing the mobility.
 
             Parameters:
                     url (str): github url of the dataframe we want to save, i.e. 'https://raw.githubusercontent.com/keivan-amini/simplified-covid-model/main/rilevazione-autoveicoli-tramite-spire-anno-2020_header_mod.csv'
@@ -74,7 +70,6 @@ total_mobility = smoothed_total/smoothed_total.max() #normalization
 
 
 #Plots
-
 plt.scatter(days, total_mobility, label = "Mobility", color = "green", marker = "o", s = 4)
 plt.scatter(m_days, m_vals, label = 'Social Activity', color ='deeppink', marker = "+", s = 60)
 plt.axvline(x=366, color='darkturquoise', ls='dotted', label = 'End of the year')
